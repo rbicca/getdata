495d48c7aa5779440774388d665bbe496237ed7a
Ronaldo Melchiades Bicca <ronaldo@softkuka.com.br>
2020-07-04 20:07:20 -0300

    Simple Examples

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 
 import './App.css';
 
 function App() {
-
-  const [data, setData] = useState({hits: []});
-
-  useEffect(() => {
-    async function fetchData(){
-      const result = await axios('https://hn.algolia.com/api/v1/search?query=redux');
-      setData(result.data);  
-    }
-    fetchData();
-  });
-
-
+  const [query, setQuery] = useState('Visual Basic');
+  const [{data, isLoading, isError}, doFetch ] = useHackerNewsApi();
   return (
     <div className="App">
       <h1>Getting data</h1>
       <hr/>
-      <ul>
-        {data.hits.map(item => {
-          console.log(item);
-          return (
-          <li key={item.objectID}>
-          <a href={item.url}><p>{item.title}</p></a>
-          </li>
-        )})}
-      </ul>
+      <Fragment>
+        <form
+          onSubmit = {e => {
+            doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+            e.preventDefault();
+          }}
+        >
+          
+          <input type="text" value={query} onChange={e => setQuery(e.target.value)}/>
+          <button type="submit">Get</button>
+        </form>
+      </Fragment>
+      
+      { isError && <div> Something went wrong </div> }
+      { isLoading ? (
+        <div>Loading...</div>
+        ) : (
+          <ul>
+          {data.hits.map(item => {
+            console.log(item);
+            return (
+            <li key={item.objectID}>
+              <a href={item.url}><p>{item.title}</p></a>
+            </li>
+          )})}
+        </ul>
+      )
+    }
     </div>
   );
 }
 
 export default App;
+
+const useHackerNewsApi = () => {
+
+  const [data, setData] = useState({hits: []});
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=Visual Basic');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+
+    async function fetchData() {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch(error) {
+        setIsError(true);
+      }
+      
+      setIsLoading(false);  
+    }
+
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError}, setUrl];
+}
